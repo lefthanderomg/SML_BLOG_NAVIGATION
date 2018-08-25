076571d19549baee80b71f85b379271c38963fb8
AndreyMurzin <lefthanderomg@gmail.com>
2018-08-25 16:43:00 +0300

    bottom navigation

---
+++ app/src/main/java/andrey/murzin/navigation/bottom_navigation/BottomActvitity.java
@@ -0,0 +1,27 @@
+package andrey.murzin.navigation.bottom_navigation;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+import andrey.murzin.navigation.R;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+public class BottomActvitity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
+    }
+}
